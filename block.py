d2c73a5c6bb810def56a4659eea4d9413d98c2b8
Daria Litvintseva <litv.daria@gmail.com>
2018-12-12 13:49:21 +0300

    Added transactions and wallets logic

--- block.py
+++ block.py
@@ -47,21 +47,24 @@ class Block:
     # timestamp
     # hash
     # previous hash
-    # message
+    # transactions
     # nonce
+    # merkle_root
 
-    def __init__(self, message, prev_hash):
-        self.message = message
+    def __init__(self, prev_hash):
         self.prev_hash = prev_hash
         self.timestamp = time.time()
+
+        self.transactions = list()
+        self.merkle_root = ""
 
         self.__nonce = 0
         self.hash = self._block_hash()
 
-        self.mine_block(BlockChain.difficulty)
+        # self.mine_block(BlockChain.difficulty)
 
     def _block_hash(self):
-        line = self.prev_hash + str(self.timestamp) + str(self.__nonce) + self.message
+        line = self.prev_hash + str(self.timestamp) + str(self.__nonce) + self.merkle_root
         return Block.get_hash(line)
 
     # TODO: move from Block class to some static class
@@ -72,9 +75,32 @@ class Block:
         return res.hexdigest()
 
     def print_block(self):
-        print("Message: {0}\nTimestamp: {1}\nHash: {2}".format(self.message, self.timestamp, self.hash))
+        print("Timestamp: {0}\nHash: {1}".format(self.timestamp, self.hash))
+
+    # TODO: get normal merkle tree
+    def __get_merkle_root(self):
+        prev_layer = list()
+        for tx in self.transactions:
+            prev_layer.append(tx.id)
+
+        new_layer = prev_layer
+        count = len(self.transactions)
+        while count > 1:
+            new_layer = list()
+            for i in range(1, len(prev_layer)):
+                new_layer.append(Block.get_hash(prev_layer[i - 1] + prev_layer[i]))
+            count = len(new_layer)
+            prev_layer = new_layer
+
+        merkle_root = ''
+        if len(new_layer) == 1:
+            merkle_root = new_layer[0]
+
+        self.merkle_root = merkle_root
+        return merkle_root
 
     def mine_block(self, difficulty):
+        self.__get_merkle_root()
         target = "0"*difficulty
         tmp_hash = self.hash
 
@@ -84,6 +110,18 @@ class Block:
 
         self.hash = tmp_hash
         print("Successfully mined {0}".format(tmp_hash))
+
+    def add_transaction(self, transaction):
+        if transaction is None:
+            return False
+
+        if self.prev_hash is not '0':
+            if not transaction.process_transaction():
+                print('Transaction is discarded')
+                return False
+        self.transactions.append(transaction)
+        print('Transaction was added to a block')
+        return True
 
 
 class BlockChain:
@@ -126,26 +164,46 @@ class BlockChain:
 class Wallet:
     # public key
     # private key
+    # UTO - unspent transactions of this owner
 
     def __init__(self):
         self.__generate_key_pair()
+        self.UTO = dict()
 
     def __generate_key_pair(self):
         self.private_key, self.public_key = SshPair.get_public_private()
 
     def get_balance(self):
         res = 0
-        for tx in BlockChain.UTO.items():
+        for tx in BlockChain.UTO.values():
             if tx.my_coin(self.public_key):
+                self.UTO[tx.id] = tx
                 res += tx.value
 
         return res
 
-    def create_transaction(self, recipient, value):
+    def send_money(self, recipient, value):
         if self.get_balance() < value:
             print("Can not send a transaction, not enough money")
-            return False
+            return None
 
+        inputs = list()
+        money_sum = 0
+
+        for tx in self.UTO.values():
+            money_sum += tx.value
+            inputs.append(TransactionInput(tx.id))
+
+            if money_sum > value:
+                break
+
+        new_tx = Transaction(self.public_key, recipient, value, inputs)
+        new_tx.generate_signature(self.private_key)
+
+        for tx in inputs:
+            self.UTO.pop(tx.output_id)
+
+        return new_tx
 
 
 class TransactionOutput:
@@ -159,7 +217,7 @@ class TransactionOutput:
         self.value = value
         self.parent_transaction_id = parent_transaction_id
 
-        line = recipient.to_string() + str(value) + str(parent_transaction_id)
+        line = recipient.to_pem().decode('utf-8') + str(value) + str(parent_transaction_id)
         self.id = Block.get_hash(line)
 
     def my_coin(self, public_key):
@@ -200,8 +258,8 @@ class Transaction:
     # TODO: remove get hash from Block class
     def __calculate_hash(self):
         self.sequence += 1
-        line = self.sender.decode('utf-8') + \
-            self.recipient.decode('utf-8') + \
+        line = self.sender.to_pem().decode('utf-8') + \
+            self.recipient.to_pem().decode('utf-8') + \
             str(self.value) + str(self.sequence)
 
         return Block.get_hash(line)
@@ -253,31 +311,81 @@ class Transaction:
             if tx.UTO is not None:
                 BlockChain.UTO.pop(tx.output_id)
 
+        return True
+
 
 def main():
+    # print("TEST #1")
+    # # initial block
+    # block = Block('test', '0')
+    # chain = BlockChain(4)
+    #
+    # chain.add_to_chain(block)
+    # chain.add_to_chain(Block('test1', str(chain.get_last().hash)))
+    # chain.add_to_chain(Block('test2', str(chain.get_last().hash)))
+    #
+    # chain.print_chain()
+    # print(chain.check_validity())
+    #
+    # print("TEST #2")
+    # # wallets testing
+    # wallet1 = Wallet()
+    # wallet2 = Wallet()
+    #
+    # print(wallet1.public_key, '\n', wallet1.private_key)
+    # print(wallet2.public_key, '\n', wallet2.private_key)
+    #
+    # transaction = Transaction(wallet1.public_key, wallet2.public_key, 5, None)
+    # transaction.generate_signature(wallet1.private_key)
+    # print(transaction.verify_signature(wallet1.public_key))
+
+    chain = BlockChain()
+
+    walletA = Wallet()
+    walletB = Wallet()
+    gen_wallet = Wallet()
+
+    print('Creating genesis transaction')
+    gen_tx = Transaction(gen_wallet.public_key, walletA.public_key, 100, None)
+    gen_tx.generate_signature(gen_wallet.private_key)
+    gen_tx.id = '0'
+    gen_tx.outputs.append(TransactionOutput(gen_tx.recipient, gen_tx.value, gen_tx.id))
+    BlockChain.UTO[gen_tx.outputs[0].id] = gen_tx.outputs[0]
+
+    print('Creating genesis block')
+    gen_block = Block('0')
+    gen_block.add_transaction(gen_tx)
+    chain.add_to_chain(gen_block)
+
     print("TEST #1")
-    # initial block
-    block = Block('test', '0')
-    chain = BlockChain(4)
 
-    chain.add_to_chain(block)
-    chain.add_to_chain(Block('test1', str(chain.get_last().hash)))
-    chain.add_to_chain(Block('test2', str(chain.get_last().hash)))
-
-    chain.print_chain()
-    print(chain.check_validity())
+    print("A balance: ", walletA.get_balance())
+    print("B balance: ", walletB.get_balance())
+    block1 = Block(gen_block.hash)
+    block1.add_transaction(walletA.send_money(walletB.public_key, 40))
+    chain.add_to_chain(block1)
+    print("A balance: ", walletA.get_balance())
+    print("B balance: ", walletB.get_balance())
 
     print("TEST #2")
-    # wallets testing
-    wallet1 = Wallet()
-    wallet2 = Wallet()
 
-    print(wallet1.public_key, '\n', wallet1.private_key)
-    print(wallet2.public_key, '\n', wallet2.private_key)
+    print("A balance: ", walletA.get_balance())
+    print("B balance: ", walletB.get_balance())
+    block1 = Block(gen_block.hash)
+    block1.add_transaction(walletA.send_money(walletB.public_key, 400))
+    chain.add_to_chain(block1)
+    print("A balance: ", walletA.get_balance())
+    print("B balance: ", walletB.get_balance())
 
-    transaction = Transaction(wallet1.public_key, wallet2.public_key, 5, None)
-    transaction.generate_signature(wallet1.private_key)
-    print(transaction.verify_signature(wallet1.public_key))
+    print("TEST #3")
+
+    print("A balance: ", walletA.get_balance())
+    print("B balance: ", walletB.get_balance())
+    block2 = Block(gen_block.hash)
+    block2.add_transaction(walletB.send_money(walletA.public_key, 40))
+    chain.add_to_chain(block2)
+    print("A balance: ", walletA.get_balance())
+    print("B balance: ", walletB.get_balance())
 
 
 if __name__ == "__main__":
